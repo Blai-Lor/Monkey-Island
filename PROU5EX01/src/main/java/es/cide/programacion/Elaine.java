f641370be51bc9af2290ff35bd0b1a5ab83b2245
Blai Lorente <lorentefusterblai@gmail.com>
2026-02-23 17:42:35 +0200

    Remove commented author information from Elaine.java

--- PROU5EX01/src/main/java/es/cide/programacion/Elaine.java
+++ PROU5EX01/src/main/java/es/cide/programacion/Elaine.java
@@ -1,6 +1,3 @@
-//Nombre: Blai Lorente Fuster
-//DNI: 43482846N
-//Fecha: 09/12/2025
 package es.cide.programacion;
 
 import java.util.Random;
@@ -63,3 +60,4 @@ public class Elaine extends Heroi implements Fight, Speak{
     public void insultar(){
     }
 }
+
